1046632ac6b96bba8ba57ce7a1995bd4de950583
Mihkel Jõesalu <mihkel.joesalu@voco.ee>
2024-09-28 11:35:13 +0300

    spämmfilter

--- yl2.5.js
+++ yl2.5.js
@@ -4,3 +4,20 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 })
+
+rl.question("Sisestage kirja teema pealkiri: ", pealkiri =>{
+    rl.question("Sisestage kirja suurus (MB): ", suurus =>{
+        rl.question("Kas kirjaga on kaasas fail(jah/ei)?:", manus =>{
+            //let
+            //let 
+            //let 
+
+            if (pealkiri == 0 || suurus > 1 || manus == "jah" || manus == "Jah" || manus == "JAH"){
+                console.log("See kiri on spämm!!!")
+            } else {
+                console.log("See kiri ei ole spämm")
+            } 
+rl.close()
+        })
+    })
+})    
